8c9702cb9c73e9aea08ab4d377e530662e6ab9e1
malex1306 <alexandremarcel350@gmail.com>
2024-07-07 20:19:53 +0200

    updates

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,8 +3,16 @@ import "./header.css"
 
 export default function Header() {
   return (
-    <div className='header'>
-        header
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Mommy's</span>
+        <span className="headerTitleLg">Bookblog</span>
+      </div>
+      <img 
+        className="headerImg"
+        src={`${process.env.PUBLIC_URL}/Bücher.jpg`} 
+        alt="Bücher" 
+      />  
     </div>
   )
 }
